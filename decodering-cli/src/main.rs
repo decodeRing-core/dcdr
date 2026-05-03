ed79279d54a1d403b3cf9fdd8a61764fbaeb03c5
Ricardo <rtm@relativemail.com>
2026-05-03 21:13:00 +0700

    Add decodering-cli workspace

---
+++ decodering-cli/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+use decodering_core::plugin::error::PluginError;
+use decodering_core::plugin::orchestrator::Orchestrator;
+use dotenvy::dotenv;
+
+fn main() -> Result<(), PluginError> {
+    println!("decodeRing CLI starting...");
+    dotenv().ok();
+    let plugin_directory = env::var("PLUGIN_DIRECTORY").expect("PLUGIN_DIRECTORY must be set");
+
+    let mut orchestrator = Orchestrator::new();
+    let out = orchestrator.load_wasm_plugins_from_dir(&plugin_directory);
+    if let Err(error) = out {
+        return Err(error);
+    };
+
+    println!("decodeRing core ready. Accepting OSL requests.");
+
+    println!("decodeRing exiting.");
+    Ok(())
+}
